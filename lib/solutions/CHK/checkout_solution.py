0bc62ee6f63b8d909bc2370f4ec19b7daccd662d
sbx_user1051 <sbx_user1051@169.254.156.241>
2023-08-02 20:09:42 +0000

    Wed Aug 02 20:09:42 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -31,18 +31,18 @@ def checkout(skus):
     total = 0
     
     a = count["A"]
+    # mod 3 * 130
+    # a - (above * 3) * 50
     while a > 0:
         if a >= 3:
             total += 130
             a -= 3
         else:
             total += 50
-            a -= 1
-
-    while 
-    
+            a -= 1  
 
     return total
+
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,9 +3,13 @@ from solutions.CHK import checkout_solution
 
 
 class TestCheckout():
+    def test_one_a(self):
+        assert(checkout_solution.checkout("A") == 50)
+
     def test_price(self):
         # Assume input is like: "ABCABC" maybe?
         assert(checkout_solution.checkout("ABC") == 100)
 
     def test_invalid(self):
         assert(checkout_solution.checkout("F") == -1)
+
