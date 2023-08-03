4c2f41e36b5449569f1603b72845938774909693
sbx_user1051 <sbx_user1051@169.254.156.241>
2023-08-03 02:19:24 +0000

    Thu Aug 03 02:19:24 UTC 2023

--- lib/solutions/CHK/shop.py
+++ lib/solutions/CHK/shop.py
@@ -5,11 +5,13 @@ from solutions.CHK.item import Item
 
 SKU = NewType("SKU", str)
 
+
 @dataclass
-class Group():
+class Group:
     skus: [SKU]
     size: int
     price: int
+
 
 class Shop:
     def __init__(self):
@@ -37,7 +39,7 @@ class Shop:
         self.groups.append(group)
 
     def run_groups(self, skus: [SKU], group_size: int, price: int):
-        count = 0
+        groups_total = 0
         seen = {}
         for sku in skus:
             if self.items[sku].amount > 0:
@@ -47,8 +49,12 @@ class Shop:
                     seen[sku] += 1
 
             if sum([x for x in seen.values()]) == group_size:
-                
+                groups_total += group_size * price
 
+                for sku in seen.keys():
+                    self.items[sku] -= 1
+
+        return groups_total
 
     def add_effect(self, affected: SKU, cause: SKU, amount: int):
         """
@@ -72,6 +78,7 @@ class Shop:
     def run_effects(self):
         for effect in self.effects:
             effect()
+
 
 
 
